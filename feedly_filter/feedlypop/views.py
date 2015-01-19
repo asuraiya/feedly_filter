d765e1da39b571a0189749985afb5254c59600cb
Abhinav Suraiya <absuraiya@gmail.com>
2015-01-19 00:01:23 -0500

    introduce a view

--- feedly_filter/feedlypop/views.py
+++ feedly_filter/feedlypop/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
+import requests
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the feedlypop index.")
+
+    return render(request, "feedlypop/index.html", context)
