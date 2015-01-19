0ad01b68f215b60f94d84b8f2d7620d1af326a45
Abhinav Suraiya <absuraiya@gmail.com>
2015-01-19 00:02:08 -0500

    fix index

--- feedly_filter/feedlypop/views.py
+++ feedly_filter/feedlypop/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 import requests
 
 def index(request):
-
+    context = {
+        'stories': [1,2,3]
+    }
     return render(request, "feedlypop/index.html", context)
